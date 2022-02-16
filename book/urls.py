879237c5ecd0c79e87d607819b25ea661e18f3c6
Rasul <Rasulmoldaly@gmail.com>
2022-02-16 20:17:43 +0600

    homework3

--- book/urls.py
+++ book/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'books'
+
 urlpatterns = [
-    path('books/', views.book_all, name='book_all'),
-    path('books/<int:id>/', views.book_detail, name='book_detail')
+    path('books/', views.book_all, name='book_list'),
+    path('books/<int:id>/', views.book_detail, name='book_detail'),
+    path("add-book/", views.add_book, name="add_book")
 ]
--- book/views.py
+++ book/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from . import  models
 from django.shortcuts import get_object_or_404
+from . import  forms
+from django.shortcuts import reverse, redirect
 
 def book_all(request):
     book = models.Book.objects.all()
@@ -10,3 +12,14 @@ def book_detail(reguest,id):
     book_id = get_object_or_404(models.Book, id=id)
     comment_id = models.BookFeedback.objects.filter(foreign_key_id=id)
     return render(reguest, 'book_detail.html', {'book':book_id, 'comment':comment_id})
+
+def add_book(request):
+    if request.method == "POST":
+        form = forms.BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse("books:book_list"))
+            # return HttpResponse("Show Created Successfully")
+    else:
+        form = forms.BookForm()
+    return render(request, "add_book.html", {"form": form})
